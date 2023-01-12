ee2b142f6c5f13a2e4a0bbeeeb0f516c87ad12c5
HARUKI KOH <harukikoh@6300l-135262-w.workspace.unimelb.edu.au>
2023-01-12 13:07:24 +1100

    Bug fixes: It sends message now

--- Roomba Controller/src/main.cpp
+++ Roomba Controller/src/main.cpp
@@ -4,30 +4,55 @@
 #include "potentiometer.h"
 
 void setup() {
+
+  Serial.begin(9600);
+  Serial.println("setup");
+
   // Sets up the transmitter 
   vw_set_tx_pin(TRANSMIT_PIN);
   vw_setup(BITS_PER_SECOND);
-  vw_set_ptt_pin(TRANSMIT_ENABLE_PIN);
+  // vw_set_ptt_pin(TRANSMIT_ENABLE_PIN);
 
   pinMode(LED_BUILTIN, OUTPUT);
+
 }
+
 
 void loop() {
   // Reference: https://www.pjrc.com/teensy/td_libs_VirtualWire.html 
 
   // Reads the potentiometers for Motors 1 and 2
-  int mappedDataForM1 = readPotentiometer(POTENTIOMETER_PIN_FOR_M1);
-  int mappedDataForM2 = readPotentiometer(POTENTIOMETER_PIN_FOR_M2);
-  int mappedDataForMotors[] = {mappedDataForM1, mappedDataForM2};
+  // int mappedDataForM1 = readPotentiometer(POTENTIOMETER_PIN_FOR_M1);
+  // int mappedDataForM2 = readPotentiometer(POTENTIOMETER_PIN_FOR_M2);
+  // int mappedDataForMotors[] = {mappedDataForM1, mappedDataForM2};
+
+  byte mappedDataForMotors[] = {100, 200};
+
+  Serial.print("Transmitter sent a message: ");
+
+
+  // mappedDataForMotors[0] = (i + 1) * 100 * pow(-1, i);
+  // mappedDataForMotors[1] = (i + 1) * 100 * pow(-1, i);
+
+  // if (i == 1) {
+  //   mappedDataForMotors[1] = mappedDataForMotors[1] * -1;
+  // }
 
   // The LED light is flashed on and off to indicate data transmission
   digitalWrite(LED_BUILTIN, HIGH);
 
   // Sends the speed and direction info about Motors 1 and 2
-  vw_send((uint8_t *) mappedDataForMotors, 2);
+  Serial.println(vw_send(mappedDataForMotors, 2));
   vw_wait_tx();
-  delay(100);
+
+  delay(1000);
 
   digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
 
+  // Serial.println("World");
+  // delay(1000);
+
+  
 }
